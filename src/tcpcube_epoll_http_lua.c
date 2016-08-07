d757a1e94b4d46e715620082d769fae5e5286d48
Byeonggon Lee <gonny952@gmail.com>
2016-08-07 19:27:37 +0900

    Basic template

---
+++ src/tcpcube_epoll_http_lua.c
@@ -0,0 +1,35 @@
+#include <err.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <tcpcube/tcpcube_module.h>
+#include <libgonc/gonc_list.h>
+
+int tcpcube_epoll_http_module_init(struct tcpcube_module_args* module_args, struct tcpcube_module_list* module_list)
+{
+    struct tcpcube_module* module = malloc(sizeof(struct tcpcube_module));
+    GONC_LIST_ELEMENT_INIT(module);
+    GONC_LIST_APPEND(module_list, module);
+    return 0;
+}
+
+int tcpcube_epoll_http_module_tlinit(struct tcpcube_module* module)
+{
+    return 0;
+}
+
+int tcpcube_epoll_http_module_service(struct tcpcube_module* module, int* client_socket)
+{
+    return 0;
+}
+
+int tcpcube_epoll_http_module_tldestroy(struct tcpcube_module* module)
+{
+    warnx("tcpcube_epoll_http_module_tldestroy()");
+    return 0;
+}
+
+int tcpcube_epoll_http_module_destroy(struct tcpcube_module* module)
+{
+    free(module);
+    return 0;
+}
