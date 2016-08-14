5145192b46e62a1457726ad813d72b239e448512
Byeonggon Lee <gonny952@gmail.com>
2016-08-14 21:59:26 +0900

    successful test

--- src/tucube_epoll_http_lua.c
+++ src/tucube_epoll_http_lua.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 #include <tucube/tucube_module.h>
 #include <libgonc/gonc_list.h>
-#include "../../tucube_epoll/src/tucube_epoll_cldata.h"
+#include "../../tucube_tcp_epoll/src/tucube_tcp_epoll_cldata.h"
 
 int tucube_epoll_http_module_init(struct tucube_module_args* module_args, struct tucube_module_list* module_list)
 {
@@ -18,27 +18,68 @@ int tucube_epoll_http_module_tlinit(struct tucube_module* module, struct tucube_
     return 0;
 }
 
-int tucube_epoll_http_module_on_method()
+int tucube_epoll_http_module_clinit(struct tucube_module* module, struct tucube_tcp_epoll_cldata_list* cldata_list)
 {
+    return 0;
 }
 
-int tucube_epoll_http_module_on_url()
+
+int tucube_epoll_http_module_on_method(char* token, ssize_t token_offset)
 {
+    char* method = malloc(sizeof(token_offset) + 1);
+    method[token_offset] = '\0';
+    memcpy(method, token, token_offset);
+    warnx("%s", method);
+    free(method);
+    return 0;
 }
 
-int tucube_epoll_http_modue_on_protocol()
+int tucube_epoll_http_module_on_url(char* token, ssize_t token_offset)
 {
+    char* url = malloc(sizeof(token_offset) + 1);
+    url[token_offset] = '\0';
+    memcpy(url, token, token_offset);
+    warnx("%s", url);
+    free(url);
+    return 0;
 }
 
-int tucube_epoll_http_module_on_header_field()
+int tucube_epoll_http_module_on_protocol(char* token, ssize_t token_offset)
 {
+    char* protocol = malloc(sizeof(token_offset) + 1);
+    protocol[token_offset] = '\0';
+    memcpy(protocol, token, token_offset);
+    warnx("%s", protocol);
+    free(protocol);
+    return 0;
 }
 
-int tucube_epoll_http_module_on_header_value()
+int tucube_epoll_http_module_on_header_field(char* token, ssize_t token_offset)
 {
+    char* header_field = malloc(sizeof(token_offset) + 1);
+    header_field[token_offset] = '\0';
+    memcpy(header_field, token, token_offset);
+    warnx("%s", header_field);
+    free(header_field);
+    return 0;
 }
 
-int tucube_epoll_http_module_service(struct tucube_module* module, int* client_socket)
+int tucube_epoll_http_module_on_header_value(char* token, ssize_t token_offset)
+{
+    char* header_value = malloc(sizeof(token_offset) + 1);
+    header_value[token_offset] = '\0';
+    memcpy(header_value, token, token_offset);
+    warnx("%s", header_value);
+    free(header_value);
+    return 0;
+}
+
+int tucube_epoll_http_module_service(struct tucube_module* module)
+{
+    return 0;
+}
+
+int tucube_epoll_http_module_cldestroy(struct tucube_module* module, struct tucube_tcp_epoll_cldata* cldata)
 {
     return 0;
 }
--- src/tucube_epoll_http_lua.h
+++ src/tucube_epoll_http_lua.h
@@ -2,21 +2,21 @@
 #define _TUCUBE_EPOLL_HTTP_LUA_H
 
 #include <tucube/tucube_module.h>
-#include "../../tucube_epoll/src/tucube_epoll_cldata.h"
+#include "../../tucube_tcp_epoll/src/tucube_tcp_epoll_cldata.h"
 
 int tucube_epoll_http_module_init(struct tucube_module_args* module_args, struct tucube_module_list* module_list);
 int tucube_epoll_http_module_tlinit(struct tucube_module* module, struct tucube_module_args* module_args);
-int tucube_epoll_http_module_clinit();
+int tucube_epoll_http_module_clinit(struct tucube_module* module, struct tucube_tcp_epoll_cldata_list* cldata_list);
 
-int tucube_epoll_http_module_on_method();
-int tucube_epoll_http_module_on_url();
-int tucube_epoll_http_modue_on_protocol();
-int tucube_epoll_http_module_on_header_field();
-int tucube_epoll_http_module_on_header_value();
+int tucube_epoll_http_module_on_method(char* token, ssize_t token_offset);
+int tucube_epoll_http_module_on_url(char* token, ssize_t token_offset);
+int tucube_epoll_http_module_on_protocol(char* token, ssize_t token_offset);
+int tucube_epoll_http_module_on_header_field(char* token, ssize_t token_offset);
+int tucube_epoll_http_module_on_header_value(char* token, ssize_t token_offset);
 
-int tucube_epoll_http_module_service(struct tucube_module* module, int* client_socket);
+int tucube_epoll_http_module_service(struct tucube_module* module);
 
-int tucube_epoll_http_module_cldestroy();
+int tucube_epoll_http_module_cldestroy(struct tucube_module* module, struct tucube_tcp_epoll_cldata* cldata);
 int tucube_epoll_http_module_tldestroy(struct tucube_module* module);
 int tucube_epoll_http_module_destroy(struct tucube_module* module);
 
