600e1c84f19e56e653bb74cf0fafcaa2b42b5131
Byeonggon Lee <gonny952@gmail.com>
2017-07-24 13:29:19 +0000

    Commit

---
+++ test/test.lua
@@ -0,0 +1,10 @@
+function onRequestFinish(client)
+    for k, v in pairs(client.request) do
+        print(k, v)
+    end
+    client.response:writeVersion(client.request.versionMajor, versionMinor)
+    client.response:writeStatusCode(200)
+    client.response:writeStringHeader("Content-Length", 5)
+    client.response:writeCrLf()
+    client.response:writeBytes("HELLO")
+end
