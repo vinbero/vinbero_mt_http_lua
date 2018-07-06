f410745e911dec06051189266605668a54756bad
Byeonggon Lee <gonny952@gmail.com>
2018-07-07 01:17:44 +0900

    Modify test/test.lua

--- test/test.lua
+++ test/test.lua
@@ -1,14 +1,18 @@
 function onRequestFinish(client)
+    local content = {}
+    table.insert(content, "<h1>Request Info</h1>")
     for k, v in pairs(client.request) do
-        print(k, v)
+        table.insert(content, "<div>" .. k .. ": " .. v .. "</div>")
     end
+    table.insert(content, "<hr/>")
     for k, v in pairs(client.request.headers) do
-        print(k, v)
+        table.insert(content, "<div>" .. k .. ": " .. v .. "</div>")
     end
-
+    content = table.concat(content)
     client.response:writeVersion(client.request.versionMajor, client.request.versionMinor)
     client.response:writeStatusCode(200)
-    client.response:writeStringHeader("Content-Length", 5)
+    client.response:writeStringHeader("Content-Type", "html")
+    client.response:writeStringHeader("Content-Length", #content)
     client.response:writeCrLf()
-    client.response:writeBytes("HELLO")
+    client.response:writeBytes(content)
 end
