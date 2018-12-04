b1f3193521ff1aef70ce2d912f9f8e817c4d0bd8
Byeonggon Lee <gonny952@gmail.com>
2018-12-04 21:52:26 +0000

    Modified app.lua

--- app.lua
+++ app.lua
@@ -1,5 +1,6 @@
 function onRequestFinish(client)
     local content = {}
+    table.insert(content, "<!DOCTYPE html><html><head></head><body>")
     table.insert(content, "<h1>Request Info</h1>")
     for k, v in pairs(client.request) do
         table.insert(content, "<div>" .. k .. ": " .. tostring(v) .. "</div>")
@@ -8,6 +9,7 @@ function onRequestFinish(client)
     for k, v in pairs(client.request.headers) do
         table.insert(content, "<div>" .. k .. ": " .. v .. "</div>")
     end
+    table.insert(content, "</body></html>")
     content = table.concat(content)
     client.response:writeVersion(client.request.versionMajor, client.request.versionMinor)
     client.response:writeStatusCode(200)
